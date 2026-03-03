d8b33c5593e55c4966073d374a7e18df904d67f7
Dileep Hegde <dileephegde96@gmail.com>
2026-03-04 00:49:43 +0530

    refactor: Rename application and test classes for clarity

--- src/main/java/com/dileephegde/loanapplicationevaluator/LoanApplicationEvaluator.java
+++ src/main/java/com/dileephegde/loanapplicationevaluator/LoanApplicationEvaluator.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoanApplicationEvaluatorApplication {
+public class LoanApplicationEvaluator {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoanApplicationEvaluatorApplication.class, args);
+        SpringApplication.run(LoanApplicationEvaluator.class, args);
     }
 
 }
--- src/test/java/com/dileephegde/loanapplicationevaluator/LoanApplicationEvaluatorTests.java
+++ src/test/java/com/dileephegde/loanapplicationevaluator/LoanApplicationEvaluatorTests.java
@@ -1,9 +1,8 @@
 package com.dileephegde.loanapplicationevaluator;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoanApplicationEvaluatorApplicationTests {
+class LoanApplicationEvaluatorTests {
 
 }
